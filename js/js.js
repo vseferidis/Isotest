47bfca902b9c542174f88cf7908d493679daf21d
Victor Seferidis <vseferidis@ue-macbook-13-share.local>
2020-01-07 15:57:22 +0100

    restructure

--- js/js.js
+++ js/js.js
@@ -1,4 +1,5 @@
-let display = document.querySelector('.calc-display');
+let active = document.querySelector('.calc-active');
+let history = document.querySelector('.calc-history');
 let keyPress = document.querySelector('.calc-keys').addEventListener('click', buttonClick);
 let key;
 let action;
@@ -6,49 +7,42 @@ let action;
 let n1 = '';
 let n2 = '';
 let operator;
-let result = 0;
+let result = '';
+active.innerHTML = "0";
 
 function buttonClick(e) {
     key = parseFloat(e.target.innerHTML);
 
-    n1 = parseFloat(n1+""+key);
+    n1 = parseFloat(n1 + "" + key);
     console.log(n1);
     action = e.target.dataset.value;
-    display.innerHTML = n1;
+    active.innerHTML = n1;
     if (action == "div") {
-        operator = "/";
-        if (n2 == '') {
-            n2 = n1;
-            n1 = '';
-        } else {
-            calculate(n1, n2, operator);
-            n2 = result;
-            n1 = '';
-            console.log(result);
-        }
-    } else if ( action == "times") {
-        operator == action;
-        if (n2 =='') {
-            n2 = n1;
-            n1 = '';
-        } else {
-            n2 = n2 * n1;
-        }
-        result = n2;
-        display.innerHTML = result;
-    } else if ( action == "clear") {
+        operator = " / ";
+        result = result + n1;
+        history.innerHTML = result + operator;
+        active.innerHTML = "0";
+        result += operator;
+        n1 = '';
+        return result;
+    } else if (action == "times") {
+        result = result + n1 + " * ";
+        history.innerHTML = result;
+        active.innerHTML = "0";
+        operator = "*";
+        n1 = '';
+        return result;
+    } else if (action == "clear") {
         n1 = '';
         n2 = '';
         operator = null;
-        display.innerHTML = 0;
-    } else if ( action == "equals") {
-        
-    }
-    console.log(operator);
-   function calculate(n1, n2, operator) {
-        result = parseFloat(n1+operator+n2);
-        display.innerHTML = result;
+        active.innerHTML = 0;
+    } else if (action == "equals") {
+
+        console.log(eval(result));
         return result;
     }
+
+
     return n1, n2, operator
 }
